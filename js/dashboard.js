7325c0c761d583e899a770bd2d0795c76eca1694
Chelsea Yuan <chelsea.yuan@att.net>
2022-10-16 04:33:33 -0500

    dashboard updates

--- js/dashboard.js
+++ js/dashboard.js
@@ -32,14 +32,13 @@ function updateDashboard() {
 /***  showDonations(): shows all donations on the dashboard                                        ***/
 /*****************************************************************************************************/
 function showDonations() {
-  console.log("show donations")
+  console.log("show donations");
   let charity_arr, total_arr, group_share_arr;
 
-  const data = { token: csrf_token };
   //get arrays with all events stored
-  fetch("getallevents.php", {
+  fetch("dashboard.php", {
     method: "POST",
-    body: JSON.stringify(data),
+    body: JSON.stringify(),
     headers: { "content-type": "application/json" },
   })
     .then((response) => response.json())
@@ -48,56 +47,39 @@ function showDonations() {
         donationname_arr = data.eventid_arr;
         eventdate_arr = data.eventdate_arr;
         group_share_arr = data.group_share_arr;
-
-        let donation_container = document.getElementById("donation-cards");
-
+      
         //create card for each donation
+        let donation_container = document.getElementById("donation-cards");
         for (let i = 0; i < eventdate_arr.length; i++) {
           let card = document.createElement("div").classList.add("card");
           let donor_name = document.createTextNode(donationname_arr[i]);
           card.appendChild(donor_name);
-          card.appendChild(createLevelSvg(2));
+
+          var img = document.createElement("img");
+
+          if (reward_data <= 24) {
+            img.src = "images/reward_images/level0.jpeg";
+          } else if (reward_data <= 49) {
+            img.src = "images/reward_images/level1.jpeg";
+          } else if (reward_data <= 74) {
+            img.src = "images/reward_images/level2.jpeg";
+          } else if (reward_data <= 99) {
+            img.src = "images/reward_images/level3.jpeg";
+          } else if (reward_data <= 499) {
+            img.src = "images/reward_images/level4.jpeg";
+          } else {
+            img.src = "images/reward_images/level5.jpeg";
+          }
 
           donation_container.append(card);
         }
+
+
+        //create card for total balance
+        let balance_container = document.getElementById("donut-balance");
       }
     })
     .catch((err) => console.error(err));
-}
-
-function createLevelSvg(reward_data) {
-  var svgns = "http://www.w3.org/2000/svg";
-  var container = document.createElementNS(svgns, "SVG");
-  for (var x = 0; x < 500; x += 50) {
-    for (var y = 0; y < 300; y += 50) {
-      var circle = document.createElementNS(svgns, "circle");
-      circle.setAttributeNS(null, "cx", x);
-      circle.setAttributeNS(null, "cy", y);
-      circle.setAttributeNS(null, "r", 50);
-      circle.setAttributeNS(
-        null,
-        "style",
-        "fill: none; stroke: blue; stroke-width: 1px;"
-      );
-      container.appendChild(circle);
-    }
-  }
-
-  /*
-  if (reward_data <= 25) {
-    return 1;
-  } else if (reward_data <= 50) {
-    return 2;
-  } else if (reward_data <= 75) {
-    return 3;
-  } else if (reward_data <= 100) {
-    return 4;
-  } else {
-    return 5;
-  }
-  */
-
-  return container;
 }
 
 /**********************************************************************************************/
@@ -107,27 +89,6 @@ document
   .getElementsByClass("view_details")
   .addEventListener("click", viewDetails, false);
 
-function shareEvent() {
-  let share_with_username = document.getElementById("share_username").value;
-  let event_id = document.getElementById("display_id").innerHTML;
-
-  //connect to php and send variables
-
-  const data = {
-    event_id: event_id,
-    share_user: share_with_username,
-    token: csrf_token, //csrf token
-  };
-
-  fetch("shareevent.php", {
-    method: "POST",
-    body: JSON.stringify(data),
-    headers: { "content-type": "application/json" },
-  })
-    .then((response) => response.json())
-    .then(function (data) {
-      console.log(data.message);
-      updateCalendar();
-    })
-    .catch((err) => console.error(err));
+function viewDetails() {
+  console.log("view details modules");
 }
