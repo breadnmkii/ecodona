7e6a7f2637f07f85ec8bd5734e7cf43367faef33
Breadn <bylueang@gmail.com>
2022-10-16 07:02:06 -0500

    css + logout page

---
+++ pages/PHP/logout.php
@@ -0,0 +1,23 @@
+<?php
+    // Check if user logged in
+    require 'helper/loginchecker.php';
+    session_destroy();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>ecodona - Donate for Good</title>
+        <link rel="stylesheet" href="../css/style.css">
+        <link rel="stylesheet" href="../css/session.css">
+    </head>
+    <body>
+        <section class="content">
+            <h2>You have successfully logged out</h2>
+            <h4>You can close this window or click <a href="../login.html">here</a> to log back in</h4>
+        </section>
+    </body>
+</html>
