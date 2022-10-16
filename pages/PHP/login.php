43543b01a485cf790da843fe82097379ca323765
f-hanna <77943310+f-hanna@users.noreply.github.com>
2022-10-16 03:23:54 -0500

    login done?
    praying screamung shitting

--- pages/PHP/login.php
+++ pages/PHP/login.php
@@ -1,22 +1,22 @@
 <?php
 require 'database.php';
 session_start(); //starts the user session
+$dashboard_redirect = "../dashboard.html";
+$login_redirect = "../login.html";
 
 //$user = "";
 
 if (isset($_POST['email']) and isset($_POST['password'])) { //checks if the username is not null
-    // $user = $_POST['user'];
-    // $pass = $_POST['pass'];
     // Use a prepared statement
-    $stmt = $mysqli->prepare("SELECT COUNT(*), userid, hashpass FROM users WHERE userid=?"); //recheck the users database
+    $stmt = $mysqli->prepare("SELECT COUNT(*), UserId, Passhash, Email FROM UserInfo WHERE Email=?"); //recheck the users database
 
     // Bind the parameter
     $email = $_POST['email'];
-    $stmt->bind_param('s', $user);
+    $stmt->bind_param('s', $email);
     $stmt->execute();
 
     // Bind the results
-    $stmt->bind_result($cnt, $user_name, $pwd_hash);
+    $stmt->bind_result($cnt, $user_id, $pwd_hash, $email_data);
     $stmt->fetch();
 
     $pwd_guess = $_POST['password'];
@@ -24,15 +24,29 @@ if (isset($_POST['email']) and isset($_POST['password'])) { //checks if the user
 
     if ($cnt == 1 && password_verify($pwd_guess, $pwd_hash)) {
         // Login succeeded!
-        $_SESSION['user'] = $user_name;
-        $_SESSION['token'] = bin2hex(random_bytes(32));
+        $_SESSION['user_id'] = $user_id;
+        // $_SESSION['username'] = $username;
+        $_SESSION['email'] = $email_data;
+
+        // $_SESSION['token'] = bin2hex(random_bytes(32));
         // Redirect to your target page
         //send the session variables to the js file
+        echo json_encode(array(
+            "success" => true,
+            "user_id" => $_SESSION['user_id']
+        ));
 
+        header("Location: $dashboard_redirect");
 
-        header("Location: dashboard.html");
     } else {
         // Login failed; redirect back to the login screen
+        echo json_encode(array(
+            "success" => false,
+            "user_id" => ''
+        ));
+
+        header("Location: $login_redirect");
+
     }
 }
 
